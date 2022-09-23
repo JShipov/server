a18a3c548da499e1f6caaff9dbd083311c26a460
yuliya shipovskaya <juliashipovskaja@gmail.com>
2022-09-23 10:26:59 +0300

    First commit

---
+++ target/debug/deps/server-d0a279ac76ca33e9.d
@@ -0,0 +1,7 @@
+C:\Users\79151\RustTest\server\target\debug\deps\server-d0a279ac76ca33e9.rmeta: src\lib.rs
+
+C:\Users\79151\RustTest\server\target\debug\deps\libserver-d0a279ac76ca33e9.rlib: src\lib.rs
+
+C:\Users\79151\RustTest\server\target\debug\deps\server-d0a279ac76ca33e9.d: src\lib.rs
+
+src\lib.rs:
---
+++ target/debug/deps/server.d
@@ -0,0 +1,6 @@
+C:\Users\79151\RustTest\server\target\debug\deps\server.exe: src\main.rs src\lib.rs
+
+C:\Users\79151\RustTest\server\target\debug\deps\server.d: src\main.rs src\lib.rs
+
+src\main.rs:
+src\lib.rs:
